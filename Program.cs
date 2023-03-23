abe10ed36d97673e46c0b3dd3894ed6cba74419c
TibidoX <vovaboch0@gmail.com>
2023-03-23 21:24:13 +0300

    RayTracing

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using RayTracing;
+
+namespace RayTracing
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(900, 900),
+                Title = "RayTracing",
+                // This is needed to run on macos
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+                window.VSync = VSyncMode.Adaptive;
+                
+                
+               
+            }
+        }
+    }
+}
